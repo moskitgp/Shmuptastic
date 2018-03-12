0fe8bd7002f28d3f872905029dfd900667b74746
moskitgp <moskitgp@gmail.com>
2018-03-12 15:07:08 +0100

    Initial commit

---
+++ Assets/Resources/Scripts/EnemyShips.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShips : MonoBehaviour {
+
+	public float horizontalSpeed;
+	public float verticalSpeed; 
+
+	public bool canShoot;
+	public float fireRate;
+	public int health;
+	public GameObject bullet;
+
+	private Rigidbody2D rb;
+	private GameObject launcherL,launcherR;
+
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+		//launcherL = transform.Find ("LauncherL").gameObject;
+		//launcherR = transform.Find ("LauncherR").gameObject;
+
+		if (canShoot) {
+			InvokeRepeating ("Shoot", fireRate,fireRate);
+		}
+	}
+	
+	void Update() {
+		rb.velocity = new Vector2 (horizontalSpeed, verticalSpeed * -1f);
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		if (collision.gameObject.tag == "Player") {
+			collision.gameObject.GetComponent<PlayerShip>().Damage();
+			Die();
+		}
+		
+	}
+
+	void Die(){
+		Destroy(gameObject);
+	}
+
+	public void Damage(){
+		health--;
+		if (health == 0)
+			Die();
+	}
+	void Shoot (){
+		Instantiate (bullet, launcherL.transform.position, Quaternion.identity);
+		Instantiate (bullet, launcherR.transform.position, Quaternion.identity);
+	}
+}
---
+++ Assets/Resources/Scripts/PlayerShip.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShip : MonoBehaviour {
+
+
+	public float speed = 25f;
+	public int health = 3;
+	public GameObject bullet;
+	public int fireRate = 10;
+
+	GameObject launcherL,launcherR;
+	Rigidbody2D rb;
+	int delay;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		launcherL = transform.Find ("LauncherL").gameObject;
+		launcherR = transform.Find ("LauncherR").gameObject;
+	}
+
+	void Update () {
+		if (Input.GetKey(KeyCode.Space) && delay>fireRate)
+			Shoot ();
+		delay++;
+	}
+	
+	void FixedUpdate () {
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		/* Vector3 movement = new Vector3 (moveHorizontal, moveVertical,0.0f);
+		rb.AddForce (movement * speed * Time.deltaTime); */ 		// Old, slugish movement method
+
+		Vector2 movement = new Vector3 (moveHorizontal, moveVertical);
+		rb.velocity = movement * speed * Time.deltaTime; 			// much better movement method and feeling
+	}
+
+	public void Damage(){
+		health--;
+		if(health == 0){
+			Destroy(gameObject);
+		}
+	}
+
+	void Shoot(){
+		delay = 0;
+		Instantiate (bullet, launcherL.transform.position, Quaternion.identity);
+		Instantiate (bullet, launcherR.transform.position, Quaternion.identity);
+	}
+}
